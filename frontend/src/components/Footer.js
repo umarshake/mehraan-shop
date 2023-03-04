8d2689f749a9aeb44e32118f1f14925a0affbe5d
umarshake <umarandis47@gmail.com>
2023-03-04 23:41:56 +0530

    footer page completed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,24 +1,91 @@
 import '../styles/Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { faTwitter, faFontAwesome, faTwitterSquare, faFacebookSquare, faInstagramSquare, faYoutubeSquare, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons';
 export default function Footer() {
     return (
-        <div className="footer">
-            <div className='footer_column1 about'>
-                <h3>Connect</h3>
-                <p>
-                    <i class="fa fa-map-marker" aria-hidden="true"></i>   Sienna Towers, Service Road, Bangalore
-                </p>
-                <div class="social">
-                    <i class="fa fa-facebook-square"></i>
-                    <FontAwesomeIcon icon="fa-brands fa-twitter-square" />
+        <footer className='footer'>
+            <div class="row primary">
+                <div class="column about">
 
-                    <i class="fa fa-linkedin-square"></i>
-                    <i class="fa fa-instagram"></i>
+                    <h3>M-Shop</h3>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                        voluptatem corporis error non,
+                    </p>
+
+                    <div class="social">
+                        <FontAwesomeIcon icon={faFacebookSquare} />
+                        <FontAwesomeIcon icon={faInstagramSquare} />
+                        <FontAwesomeIcon icon={faTwitterSquare} />
+                        <FontAwesomeIcon icon={faYoutubeSquare} />
+                        <FontAwesomeIcon icon={faWhatsappSquare} />
+                    </div>
                 </div>
+
+                <div class="column links">
+                    <h3>Some Links</h3>
+
+                    <ul>
+
+                        <li>
+                            <a href="#faq">F.A.Q</a>
+                        </li>
+                        <li>
+                            <a href="#cookies-policy">Cookies Policy</a>
+                        </li>
+                        <li>
+                            <a href="#terms-of-services">Terms Of Service</a>
+                        </li>
+                        <li>
+                            <a href="#support">Support</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+                <div class="column links">
+                    <h3>Some Links</h3>
+                    <ul>
+                        <li>
+                            <a href="#faq">F.A.Q</a>
+                        </li>
+                        <li>
+                            <a href="#cookies-policy">Cookies Policy</a>
+                        </li>
+                        <li>
+                            <a href="#terms-of-services">Terms Of Service</a>
+                        </li>
+                        <li>
+                            <a href="#support">Support</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="column subscribe">
+                    <h3>Newsletter</h3>
+                    <div>
+                        <input type="email" placeholder="Your email id here" />
+                        <button>Subscribe</button>
+                    </div>
+
+                </div>
+
             </div>
-            <div className='footer_column2 link'></div>
-            <div className='footer_column1 subscribe'></div>
-        </div>
+
+            <div class="row copyright">
+                <div class="footer-menu">
+
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    <a href="">Blog</a>
+                    <a href="">Social</a>
+
+                </div>
+                <p>Copyright &copy; 2022 Umar Developer World</p>
+            </div>
+        </footer>
     );
 }
